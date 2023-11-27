d615a57a208b15f0aee145882e046debed7bcaff
Casey Moncur <caseymonc@gmail.com>
2023-11-26 22:28:34 -0700

    Added Volume Actions

--- src/ProjectorConnection.ts
+++ src/ProjectorConnection.ts
@@ -67,7 +67,7 @@ export class ProjectorConnection {
     return await new Promise<any>((resolve, reject) => {
       const listener = (chunk: any) => {
         this.#port?.off('data', listener);
-        resolve(chunk.toString('utf8'));
+        resolve(chunk.toString("hex"));
       }
       this.#port?.on('data', listener);
       this.#port?.write(data, undefined, (err) => {
